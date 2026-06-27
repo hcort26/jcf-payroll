b2ec57797c9aaf3fcb40d3a62eba3bb34e46419b
hcort26 <144395316+hcort26@users.noreply.github.com>
2026-06-27 14:32:36 -0400

    Move Firebase config to environment variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDdx9OZl5LYagDdOhTXC4_3XsL4Hl2PC5Y",
-  authDomain: "jcf-payroll.firebaseapp.com",
-  projectId: "jcf-payroll",
-  storageBucket: "jcf-payroll.firebasestorage.app",
-  messagingSenderId: "317980613193",
-  appId: "1:317980613193:web:b94655e6dcb2195a4cbd98",
-  measurementId: "G-9SMDVN62VQ",
-};
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  };
 
 const app = initializeApp(firebaseConfig);
 
